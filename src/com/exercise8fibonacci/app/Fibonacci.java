485b698a11dfb1789f5a99f09852699ec963e557
OcarinGit <oscarsantanaalvarez@gmail.com>
2019-10-15 12:16:01 -0500

    Fibonacci

---
+++ src/com/exercise8fibonacci/app/Fibonacci.java
@@ -0,0 +1,35 @@
+package com.exercise8fibonacci.app;
+
+import java.util.Scanner;
+
+public class Fibonacci 
+{
+
+	public static void main(String[] args) 
+	{
+		int cont1=0;
+		int cont2=1;
+		long fibonacci=1;
+		int bound=0;
+		
+		Scanner input = new Scanner(System.in);
+		
+		do
+		{
+			System.out.println("Cuantos fibonaccis deseas calcular?:");
+			bound = input.nextInt();
+			if(bound<=0)
+			{
+				System.out.println("Debes de introducir un número arriba de cero...");
+			}
+		}while(bound<=0);
+		
+		for(int i=0;i<bound;i++)
+		{
+			System.out.print(fibonacci+" ");
+			fibonacci = cont1+cont2;
+			cont1=cont2;
+			cont2=(int) fibonacci;
+		}
+	}
+}
